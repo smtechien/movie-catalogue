85fea2b1d2e2d5d32abb11754e4343faf0e2813d
smtechien <sulthonrahmatullah@gmail.com>
2023-05-16 07:24:51 +0800

    adding service worker

---
+++ src/scripts/utils/sw-register.js
@@ -0,0 +1,13 @@
+const swRegister = async () => {
+  if (!('serviceWorker' in navigator)) {
+    console.log('Service worker not supported in the browser');
+    return;
+  } try {
+    await navigator.serviceWorker.register('./sw.bundle.js');
+    console.log('Service worker registered');
+  } catch (error) {
+    console.log('Failed to register service worker', error);
+  }
+};
+
+export default swRegister;
